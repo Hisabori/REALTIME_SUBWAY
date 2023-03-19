19db9e2585b556f03457204ea8ac3e950da194fc
Jeonghun HAN <101242920+Hisabori@users.noreply.github.com>
2023-03-19 15:16:58 +0900

    테스트

--- app/src/main/java/com/jh/myapplication/MainActivity.kt
+++ app/src/main/java/com/jh/myapplication/MainActivity.kt
@@ -1,3 +1,12 @@
+//Build: HAN JEONGHUN
+//DEV TOOL : Intellij IDEA ultimate
+//OS: WINDOWS 11 AND MAC OS
+//First Build: 17 mar 2023
+
+//description:
+//지하철 api 를 활용 한 실시간 열차 도착 알리미 앱
+
+
 package com.jh.myapplication
 
 import android.annotation.SuppressLint
@@ -9,6 +18,8 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -57,10 +68,10 @@ class MainActivity : AppCompatActivity() {
         searchSubway()
     }
 
-    fun onCreateOptionsMenu(menu: Menu?, ture: Boolean): Boolean {
-        val inflater = MenuInflater = menuInflater
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = MenuInflater(this)
         inflater.inflate(R.menu.main_menu, menu)
-        return ture
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -75,7 +86,7 @@ class MainActivity : AppCompatActivity() {
                     subwayArrayAdapter.notifyDataSetChanged()
                     searchSubway()
                 }, 2000)
-                return true
+                true
             }
 
             else -> super.onOptionsItemSelected(item)
@@ -102,9 +113,7 @@ class MainActivity : AppCompatActivity() {
                     noResultTextView.visibility = View.VISIBLE
                 } else {
                     noResultTextView.visibility = View.GONE
-                    subwayInfoList.forEach {
-                        subwayList.add(it)
-                    }
+                    subwayList.addAll(subwayInfoList)
                     subwayArrayAdapter.notifyDataSetChanged()
                 }
                 swipeRefreshLayout.isRefreshing = false
@@ -112,57 +121,53 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
-    private fun parseSubwayInfoList(response: String): Any {
-
-    }
-
-    private fun getHttpResponse(urlStr: String): String {
-
-        val url = URL(urlStr)
-
-        val connection = url.openConnection() as HttpURLConnection
-        connection.requestMethod = "GET"
-        connection.readTimeout = 15000
-        connection.connectTimeout = 15000
-        connection.doInput = true
+    private fun parseSubwayInfoList(response: String): ArrayList<String> {
+        val subwayInfoList = ArrayList<String>()
 
         try {
-            connection.connect()
+            val jsonObject = JSONObject(response)
+            val jsonArray: JSONArray = jsonObject.getJSONArray("realtimeArrivalList")
 
-            val responseCode = connection.responseCode
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val inputStream = connection.inputStream
-                val reader = BufferedReader(InputStreamReader(inputStream))
-                val response = StringBuffer()
+            for (i in 0 until jsonArray.length()) {
+                val item = jsonArray.getJSONObject(i)
+                val subwayName = item.getString("subwayId")
+                val arrivalMsg = item.getString("arvlMsg2")
 
-                var line: String? = reader.readLine()
-                while (line != null) {
-                    response.append(line)
-                    line = reader.readLine()
-                }
-                reader.close()
-                inputStream.close()
-
-                return response.toString()
-            } else {
-                return ""
+                val subwayInfo = "$subwayName : $arrivalMsg"
+                subwayInfoList.add(subwayInfo)
             }
         } catch (e: Exception) {
             e.printStackTrace()
-            return ""
+        }
+
+        return subwayInfoList
+    }
+
+    private fun getHttpResponse(urlStr: String): String {
+        val url = URL(urlStr)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+
+        try {
+            val input = BufferedReader(InputStreamReader(connection.inputStream))
+            val response = StringBuffer()
+
+            var line = input.readLine()
+            while (line != null) {
+                response.append(line)
+                line = input.readLine()
+            }
+
+            return response.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
         } finally {
             connection.disconnect()
         }
+
+        return ""
     }
-}
-
-private fun Any.forEach(function: () -> Boolean) {
 
 }
-
-private fun Any.isEmpty(): Boolean {
-
-}
-
-
-
---
+++ app/src/main/java/readme/CodeInfo.md
@@ -0,0 +1,35 @@
+<h1>RealtimeMetroSVC</h1>
+
+<h2>Code description
+
+
+
+<h4>해당 코드에 대한 설명
+안드로이드 APP의 MainActivity를 정의하는 Kotlin 코드로,
+
+1 ~ 16: 사용할 패키지와 라이브러리를 import 한다.
+
+18 ~ 44: AppCompatActivity 클래스를 상속 받아, MainActivity를 정의하고, EditText, ListView, TextView, SwipeRefreshLayout 등의 View 객체 / 변수 / method 가 선언되어 있다
+
+46 ~ 53: 지하철 역 이름을 입력받는 EditText, 지하철 역 정보를 보여주는 ListView, 결과가 없을 경우 보여 줄 TextView, SwipeRefreshLayout 객체를 초기화 한다.
+
+55 ~ 58: ListView에 표시할 지하철 역 정보를 저장할, subwayList 객체와 이 객체를 ListView와 연결하는 subwayArrayAdapter 객체를 선언합니다.
+
+60 ~ 66: ListView에 역 정보를 클릭 시 때 실행될 이벤트 리스너를 정의한다.
+
+68 ~ 74 SwipeRefreshLayout 객체에 새로고침 이벤트 리스너를 정의한다
+
+76: searchSubway() method를 호출한다.
+
+78 ~ 86: onCreateOptionsMenu() method를 정의한다. 해당 method는, activity에서 옵션 메뉴를 만들어 주는 역할을 한다.
+
+88 ~ 99: onOptionsItemSelected() method를 정의한다. 여기선 옵션에서 선택한 항목에 대하여 event처리를 진행 한다.
+
+101 ~ 121: 역 정보를 검색하는 searchSubway() method를 정의한다.
+
+123 ~ 153: api server 측에서 받아온 역 정보를 파싱, subwayInfoList에 저장하는 parseSubwayInfoList() method를 정의한다
+
+155 ~ 181: server에 GET 요청을 보내고, 응답을 받는 getHttpResponse() method를 정의, 이 method 서버와 통신하는 HTTPURLConnection 객체를 생성하고 연결한다. 이후, InputStream으로부터 데이터를 읽어 StringBuffer에 저장하여 반환하게 된다. 마지막으로, HTTPURLConnection 객체를 연결을 끊는다.
+
+
+
